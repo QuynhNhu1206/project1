f8b5d9f9323313bf0926cebdcf70f3bc37eea32c
quynhnhu1206 <eqnhu1206@gmail.com>
2023-05-05 21:01:50 +0700

    update project1

---
+++ 3.17/3.17/Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, s = 0;
+	cout << "\n Nhap a: ";
+	cin >> a;
+	for (int i = 1; i < a; i++)
+	{
+		if (a % i == 0)
+			s = s + i;
+	}
+	if (s == a)
+		cout << "\n day la so hoan thien";
+	else
+		cout << "\n day kp la so hoan thien";
+	return 0;
+	}
